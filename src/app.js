6165c4e50d15e8625c85094ac13b98fb3b5aacf9
max-debinsky <maksdeb.kontakt@gmail.com>
2026-06-27 18:18:44 +0200

    feat(auth): mount auth routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import applicationsRouter from './modules/applications/applications.routes.js';
+import authRouter from './modules/auth/auth.routes.js';
 import errorHandler from './middleware/error.js';
 
 const app = express();
 app.use(express.json())
 app.use('/api/applications', applicationsRouter);
+app.use('/api/auth', authRouter);
 
 app.get('/health', (req, res) => {
   res.json({status:"ok"})
